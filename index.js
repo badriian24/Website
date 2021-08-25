b241a15a3ecd64028cd5c42cc520be4b4ac19671
Bdrxzzzz <75257533+badriian24@users.noreply.github.com>
2021-08-25 15:24:55 +0700

    ✨ main file

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var port = 8080;
 
-const express = require("express")
+const listener = app.listen(port, async function() {
+  console.log(`Listening on port  ${listener.address().port}`);
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(session({secret: 'bdrxzzzz', resave: false, saveUninitialized: true, cookie: {expires: 2.16e+7}}));
+
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
